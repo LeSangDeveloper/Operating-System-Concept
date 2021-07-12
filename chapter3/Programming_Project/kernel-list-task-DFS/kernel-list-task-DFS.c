e518cb8a39350125eaebae85587e1212e20e5e91
Le Sang <lesang541191309@gmail.com>
2021-07-12 18:11:47 +0700

    fix bug list_h eeor

--- chapter3/Programming_Project/kernel-list-task-DFS/kernel-list-task-DFS.c
+++ chapter3/Programming_Project/kernel-list-task-DFS/kernel-list-task-DFS.c
@@ -3,17 +3,25 @@
 #include<linux/module.h>
 #include<linux/sched.h>
 
-struct task_struct *task;
-struct list_h *list;
+// init_task there !!! (Ubuntu Desktop 20.04 LTS)
+#include<linux/sched/task.h>
+//extern struct task_struct init_task;
 
-/* This function is called when the module is loaded. */
-int simple_init(void) {
-	printk(KERN_INFO "Loading Module\n");
+void dfs(struct task_struct *init_task) {	
+	struct task_struct *task;
+	struct list_head *list;	
 
 	list_for_each(list, &init_task->children) {
 		task = list_entry(list, struct task_struct, sibling);
                  pr_info(KERN_INFO "PID: %d\tName: %s\tState: %d", task->pid, task->comm, task->state);
 	}
+}
+
+/* This function is called when the module is loaded. */
+int simple_init(void) {
+	printk(KERN_INFO "Loading Module\n");
+
+	dfs(&init_task);
 
 	return 0;
 }
